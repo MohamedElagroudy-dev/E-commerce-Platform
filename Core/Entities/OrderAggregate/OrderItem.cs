60bb2a2bd0e62042b378c2e01536b5e6d96f48dd
MohamedElagroudy-dev <Mohamed.Elagroudy.dev@gmail.com>
2025-08-27 01:10:18 +0300

    CreateOrder method

--- Core/Entities/OrderAggregate/OrderItem.cs
+++ Core/Entities/OrderAggregate/OrderItem.cs
@@ -8,7 +8,7 @@ namespace Core.Entities.OrderAggregate
 {
     public class OrderItem :BaseEntity
     {
-        public ProductItemOrdered ItemOrdered { get; set; } = null!;
+        public ProductItemOrdered ItemOrdered { get; set; } = null!; //owned
         public decimal Price { get; set; }
         public int Quantity { get; set; }
     }
